b5c98a04cff3bcdd21b3651d5a671aa611692ef9
Michiaki Mizoguchi <michiaki.mizoguchi@gmail.com>
2020-01-19 18:34:55 +0900

    :sumrise:

---
+++ app/src/main/java/dev/mizoguche/kiqueue/MainActivity.kt
@@ -0,0 +1,19 @@
+package dev.mizoguche.kiqueue
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Log.d(this.javaClass.canonicalName, "Add feed")
+        }
+    }
+}
